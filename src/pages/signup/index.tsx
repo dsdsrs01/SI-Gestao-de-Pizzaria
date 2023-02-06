4f2590381464d97f2d829c3628de83e7363b8181
dsdsrs01 <yuribatista9876@gmail.com>
2023-02-06 19:54:39 -0300

    Tela de cadastro

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,24 +1,30 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../../styles/home.module.scss";
+import styles from "../../../styles/home.module.scss";
 
-import Logo from "../../public/sua logo.png";
-import { Input } from "../components/ui/Input/index";
-import { Button } from "../components/ui/Button/index";
+import Logo from "../../../public/sua logo.png";
+import { Input } from "../../components/ui/Input/index";
+import { Button } from "../../components/ui/Button/index";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function Signup() {
   return(
     <>
       <Head>
-        <title>Sujeito pizza - faca seu login</title>
+        <title>Faca seu cadastro agora</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={Logo} alt="Logo Sujeito Pizzaria"/>
 
         <div className={styles.login}>
+
+            <h1 className={styles.titulo}>Criando sua conta</h1>
           <form >
+            <Input 
+              placeholder="Digite seu nome"
+              type="text"
+            />
             <Input 
               placeholder="Digite seu email"
               type="text"
@@ -33,15 +39,13 @@ export default function Home() {
               loading={false}
 
             >
-              Acessar
+              Cadastrar
             </Button>
           </form>
-
-            <Link href="/signup">
-               <p className={styles.text}>Nao possui uma conta? Cadastre-se</p>
+            <Link href="/"> 
+                <p className={styles.text}>Ja possui uma conta?</p>
             </Link>
-            
-          
+                    
         </div>
       </div>
     </>
